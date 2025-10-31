7d0daffbea000fbc6f39d3824e090c59e4393402
Ecommerce <rossgaluzinskiy@gmail.com>
2025-10-31 16:04:51 +0200

    add

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const cors = require("cors");
 const dotenv = require("dotenv");
 const express = require("express");
+const bodyParser = require('body-parser');
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const payRoute = require("./routers/pay");
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api/wayforpay", payRoute);
